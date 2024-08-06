ea85d145c4f623ccb754d8c64886acb66ccbf178
Arobh Kumar <126250992+Arobh@users.noreply.github.com>
2024-08-06 19:20:03 +0530

    Create heap.cpp

---
+++ Algorithm/Sorting/Heap Sort/heap.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+void swap(int &x, int &y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+void heapify(std::vector<int> &arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(std::vector<int> &arr) {
+    int n = arr.size();
+
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i >= 0; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    std::vector<int> arr = {12, 11, 13, 5, 6, 7};
+
+    heapSort(arr);
+
+    std::cout << "Sorted array: ";
+    for (int num : arr)
+        std::cout << num << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
