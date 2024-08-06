3d26baca8fc591515b7bbac62a07a25f4459cf80
Arobh Kumar <126250992+Arobh@users.noreply.github.com>
2024-08-06 19:27:10 +0530

    Create merge.java

---
+++ Algorithm/Sorting/Merge Sort/merge.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        System.arraycopy(arr, l, L, 0, n1);
+        System.arraycopy(arr, m + 1, R, 0, n2);
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+
+            merge(arr, l, m, r);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.print("Sorted array: ");
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+    }
+}
