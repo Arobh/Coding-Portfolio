d4d63781f5739bdcf6b69cc71cd2bfdbcda2f3f8
Arobh Kumar <126250992+Arobh@users.noreply.github.com>
2024-08-10 06:19:45 +0530

    Create shell.java

---
+++ Algorithm/Sorting/Shell Sort/shell.java
@@ -0,0 +1,24 @@
+public class ShellSort {
+    static void shellSort(int[] arr) {
+        int n = arr.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+                arr[j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 54, 2, 3};
+        shellSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
