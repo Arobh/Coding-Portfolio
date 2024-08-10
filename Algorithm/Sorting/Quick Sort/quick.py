f422446719ea439ec5703a63d35877ce09a234cd
Arobh Kumar <126250992+Arobh@users.noreply.github.com>
2024-08-10 06:14:53 +0530

    Create quick.py

---
+++ Algorithm/Sorting/Quick Sort/quick.py
@@ -0,0 +1,12 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+arr = [10, 7, 8, 9, 1, 5]
+sorted_arr = quick_sort(arr)
+print(sorted_arr)
