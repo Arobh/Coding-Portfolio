9bb1da2e5e10f3646df478c29f3282c6ec08ed24
Arobh Kumar <126250992+Arobh@users.noreply.github.com>
2024-09-03 05:04:01 +0530

    Create algo.java

---
+++ Algorithm/Greedy/Prim's Algorithm/algo.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class PrimAlgorithm {
+    private static final int V = 5;
+
+    int minKey(int[] key, boolean[] mstSet) {
+        int min = Integer.MAX_VALUE, min_index = -1;
+
+        for (int v = 0; v < V; v++)
+            if (!mstSet[v] && key[v] < min) {
+                min = key[v];
+                min_index = v;
+            }
+
+        return min_index;
+    }
+
+    void printMST(int[] parent, int[][] graph) {
+        System.out.println("Edge \tWeight");
+        for (int i = 1; i < V; i++)
+            System.out.println(parent[i] + " - " + i + "\t" + graph[i][parent[i]]);
+    }
+
+    void primMST(int[][] graph) {
+        int[] parent = new int[V];
+        int[] key = new int[V];
+        boolean[] mstSet = new boolean[V];
+
+        Arrays.fill(key, Integer.MAX_VALUE);
+        Arrays.fill(mstSet, false);
+
+        key[0] = 0;
+        parent[0] = -1;
+
+        for (int count = 0; count < V - 1; count++) {
+            int u = minKey(key, mstSet);
+            mstSet[u] = true;
+
+            for (int v = 0; v < V; v++)
+                if (graph[u][v] != 0 && !mstSet[v] && graph[u][v] < key[v]) {
+                    parent[v] = u;
+                    key[v] = graph[u][v];
+                }
+        }
+
+        printMST(parent, graph);
+    }
+
+    public static void main(String[] args) {
+        PrimAlgorithm t = new PrimAlgorithm();
+        int graph[][] = new int[][]{{0, 2, 0, 6, 0},
+                                    {2, 0, 3, 8, 5},
+                                    {0, 3, 0, 0, 7},
+                                    {6, 8, 0, 0, 9},
+                                    {0, 5, 7, 9, 0}};
+
+        t.primMST(graph);
+    }
+}
