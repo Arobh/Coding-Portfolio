6470d61e75e65f31e658dbefc6649df91322caa6
Arobh Kumar <126250992+Arobh@users.noreply.github.com>
2024-10-14 15:51:09 +0530

    Create code.java

---
+++ Code 450/Arrays/6. Write a program to cyclically rotate an array by one./code.java
@@ -0,0 +1,13 @@
+// // User function Template for Java
+
+class Solution {
+    public void rotate(int[] arr) {
+        // code here
+        int n=arr.length;
+        int x=arr[n-1];
+        for(int i=n-1;i>0;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=x;
+    }
+}
